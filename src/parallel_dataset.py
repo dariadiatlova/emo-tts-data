4584f40baa6e846463c0b440a19c91e0bb7344c5
dariadiatlova <dari.diatlova@gmail.com>
2022-06-10 15:47:00 +0300

    debug

--- src/parallel_dataset.py
+++ src/parallel_dataset.py
@@ -24,7 +24,9 @@ def get_speaker_transcripts(txt_path: str, encoding_type: str) -> Dict:
             # hard coding fix, somehow first sample from txt is read like '\ufeff0012_000001'
             if string == "\ufeff0012_000001":
                 result_dict["0012_000001.wav"] = string[1]
-            result_dict[f"{string[0]}.wav"] = string[1]
+            # handle handwritten indent
+            if len(string) > 1:
+                result_dict[f"{string[0]}.wav"] = string[1]
     return result_dict
 
 

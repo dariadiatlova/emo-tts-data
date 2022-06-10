47e65486d56492633600507cf974324404bf6caa
dariadiatlova <dari.diatlova@gmail.com>
2022-06-10 15:05:38 +0300

    debug

--- src/parallel_dataset.py
+++ src/parallel_dataset.py
@@ -24,6 +24,7 @@ def get_speaker_transcripts(txt_path: str, encoding_type: str) -> Dict:
             # hard coding fix, somehow first sample from txt is read like '\ufeff0012_000001'
             if string == "\ufeff0012_000001":
                 string = "0012_000001"
+                print(string)
             result_dict[f"{string}.wav"] = line[1]
     return result_dict
 
@@ -46,7 +47,7 @@ def build_dataset(cfg):
         speaker_transcripts_dict = get_speaker_transcripts(
             f"{cfg.source_data_directory}/{speaker_id}/{speaker_id}.txt", encoding_type=speaker_encoding_type
         )
-        print(speaker_transcripts_dict.keys())
+        # print(speaker_transcripts_dict.keys())
         # each speaker has 5 folders for emotions: "Neutral", "Angry", "Happy", "Sad", "Surprise"
         for emotion in cfg.emotions:
             # each folder of emotion has a division on tran/evaluation/test in proportion 85% / 9% / 6%
